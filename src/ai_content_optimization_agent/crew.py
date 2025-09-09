2c8afaaf18e52be64e40502c13b81d64cfd3174f
Liam Banga <liam205@outlook.com>
2025-09-09 14:32:35 +1000

    Initialise repo

---
+++ src/ai_content_optimization_agent/crew.py
@@ -0,0 +1,87 @@
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai.agents.agent_builder.base_agent import BaseAgent
+from typing import List
+from crewai_tools import BrightDataWebUnlockerTool, BrightDataSearchTool
+# If you want to run a snippet of code before or after the crew starts,
+# you can use the @before_kickoff and @after_kickoff decorators
+# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+import os
+MODEL = os.getenv("MODEL")
+
+web_unlocker_tool = BrightDataWebUnlockerTool()
+serp_search_tool = BrightDataSearchTool()
+@CrewBase
+class AiContentOptimizationAgent():
+    """AiContentOptimizationAgent crew"""
+
+    agents: List[BaseAgent]
+    tasks: List[Task]
+
+    # Learn more about YAML configuration files here:
+    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+    
+    # If you would like to add tools to your agents, you can learn more about it here:
+    # https://docs.crewai.com/concepts/agents#agent-tools
+
+    @agent
+    def title_scraper_agent(self) -> Agent:
+        return Agent(
+            config=self.agents_config["title_scraper_agent"],
+            tools=[web_unlocker_tool], # <--- Web Unlocker tool integration
+            verbose=True,
+            llm=MODEL,
+        )
+
+    @task
+    def scrape_title_task(self) -> Task:
+        return Task(
+            config=self.tasks_config["scrape_title_task"],
+            agent=self.title_scraper_agent(),
+            max_retries=3,
+        )
+
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config['researcher'], # type: ignore[index]
+            verbose=True
+        )
+
+    @agent
+    def reporting_analyst(self) -> Agent:
+        return Agent(
+            config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            verbose=True
+        )
+
+    # To learn more about structured task outputs,
+    # task dependencies, and task callbacks, check out the documentation:
+    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_task'], # type: ignore[index]
+        )
+
+    @task
+    def reporting_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['reporting_task'], # type: ignore[index]
+            output_file='report.md'
+        )
+
+    @crew
+    def crew(self) -> Crew:
+        """Creates the AiContentOptimizationAgent crew"""
+        # To learn how to add knowledge sources to your crew, check out the documentation:
+        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+
+        return Crew(
+            agents=self.agents, # Automatically created by the @agent decorator
+            tasks=self.tasks, # Automatically created by the @task decorator
+            process=Process.sequential,
+            verbose=True,
+            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+        )
